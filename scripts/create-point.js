bbbf0116582c193f4797dd53ca37395eba603094
Victor <victoorqrz@gmail.com>
2020-06-03 20:35:51 -0300

    cadastro finalizado

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -2,12 +2,12 @@ function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]");
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() )
-    .then( states => {
-        for(const state of states) {
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }
-    });
+        .then(res => res.json())
+        .then(states => {
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        });
 }
 
 populateUFs();
@@ -23,15 +23,46 @@ function getCities(ev) {
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
     fetch(url)
-    .then( (res) => res.json() )
-    .then( (cities) => {    
-        citySelect.innerHTML = '<option value="">Selecione a cidade</option>';
-        for(const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-        };
+        .then((res) => res.json())
+        .then((cities) => {
+            citySelect.innerHTML = '<option value="">Selecione a cidade</option>';
+            citySelect.disabled = true;
 
-        citySelect.disabled = false;
-    })  
+            for (const city of cities) {
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+            };
+
+            citySelect.disabled = false;
+        })
 }
 
 document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+//Itens de coleta
+
+const itensToCollect = document.querySelectorAll(".itens-grid li");
+
+for (const item of itensToCollect)
+    item.addEventListener("click", handleSelectedItem);
+
+let selectedItems = [];
+const itemsToSend = document.querySelector("input[name=items]");
+
+function handleSelectedItem(event) {
+    const itemLi = event.target;
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex(item => item == itemId);
+
+    if (alreadySelected == -1) {
+        selectedItems.push(itemId);
+    } else {
+        const filteredItems = selectedItems.filter(item => item != itemId);
+
+        selectedItems = filteredItems;
+    }
+
+    itemsToSend.value = selectedItems;
+}
